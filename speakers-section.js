a1c354a514c2b49fc560ae98b2e026978c604c46
anelle-u <anelle.utegen@gmail.com>
2024-09-29 17:20:53 -0400

    ChangeN

--- speakers-section.js
+++ speakers-section.js
@@ -2,19 +2,19 @@ const [left, right] = document.querySelectorAll("#SpeakersSection .arrows button
 
 const speakers = [
     {
-        image: "/images/speakers/buck_woody.png",
+        image: "images/speakers/buck_woody.png",
         name: "Buck Woody",
         description: "Applied Data Scientist in the Azure Data Services team",
         title: "Generative AI: Concepts, Tools, and Applications for Data Professionals"
     },
     {
-        image: "/images/speakers/james_gress.png",
+        image: "images/speakers/james_gress.png",
         name: "James Gress",
         description: "Emerging Technology Director, North America Advanced Technology Centers, Accenture",
         title: "AI-Assisted Coding: Beyond Code Generation to Full Potential"
     },
     {
-        image: "/images/speakers/laurence_svekis.png",
+        image: "images/speakers/laurence_svekis.png",
         name: "Laurence Svekis",
         description: "Google Developer Expert, a web technology consultant, and an online course developer",
         title: "Power Up (Apps Script & Gemini AI)"
